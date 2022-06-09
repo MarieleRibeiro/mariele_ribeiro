42987e236662696f0ea40b896074f32c6cd88535
marieleribeiro <mariele_rbr@hotmail.com>
2022-06-09 20:18:12 -0300

    feat: add responsive

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -7,21 +7,36 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
-  
+
   body {
     font-family: Inter, sans-serif;
   }
-  
+
   h1 {
     font-size: 54px;
-    color: ${theme.colors.gray600}
+    color: ${theme.colors.gray600};
+    @media (max-width: 920px) {
+      font-size: 32px;
+    }
   }
+
   p {
     font-size: 24px;
-    color: ${theme.colors.black}
+    color: ${theme.colors.black};
+
+    @media (max-width: 920px) {
+      font-size: 16px;
+    }
   }
+
+
   h4 {
     font-size: 24px;
+
+    @media (max-width: 920px) {
+      font-size: 18px;
+    }
   }
+
 
 `
